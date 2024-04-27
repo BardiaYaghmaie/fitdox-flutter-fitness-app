0479d93efb94d6e4ce643bb74be6fd36b15fcaed
Bardia Yaghmaie <bardia.yaghmaie.81@gmail.com>
2024-04-27 15:27:47 +0330

    appbar buttons

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
@@ -17,6 +19,41 @@ class Homepage extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 0,
         centerTitle: true,
+        leading: GestureDetector(
+          onTap: (){
+            
+          },
+          child: Container(
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            child: SvgPicture.asset('assets/icons/arrow-left.svg',
+            height: 20,
+            width: 20,),
+            decoration: BoxDecoration(
+              color: Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: (){
+
+            },
+            child: Container(
+              width: 37,
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            child: SvgPicture.asset('assets/icons/dots.svg',
+            height: 5,
+            width: 5,),
+            decoration: BoxDecoration(
+              color: Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10),
+            ),
+                    ),
+          ),
+        ],
       ),
     );
   }
